bae31309b29852e09e105e21c8f63b00632ca90d
FeliciousX <feliciousx@gmail.com>
2013-08-15 16:28:56 +0800

    Refining main.js page
    o Deleted unnecessary files
    + updated package.json (run npm install)
    ~ refined tests
    + added TODOs
    ~ build new dist

--- app/scripts/services/categoryList.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('halamanHeritageApp')
-  .factory('categoryList', function ($resource) {
-    return $resource('places/:categoryId.json', {}, {
-      query: {method:'GET', params:{categoryId:'categories'}, isArray:true}
-    });
-  });
--- dist/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
+++ dist/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
@@ -2924,7 +2924,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position'])
 angular.module("template/accordion/accordion-group.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/accordion/accordion-group.html",
     "<div class=\"accordion-group\">\n" +
-    "  <div class=\"accordion-heading\" ><a class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\">{{heading}}</a></div>\n" +
+    "  <div class=\"accordion-heading\" ><a class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\"><img class=\"accordion-image\" ng-src=\"img/{{heading}}.png\" />{{heading}}</a></div>\n" +
     "  <div class=\"accordion-body\" collapse=\"!isOpen\">\n" +
     "    <div class=\"accordion-inner\" ng-transclude></div>  </div>\n" +
     "</div>");
