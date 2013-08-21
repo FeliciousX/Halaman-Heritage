56ca3f48297016dd4e9b9ab9c60f679efcb48e52
FeliciousX <feliciousx@gmail.com>
2013-08-21 14:57:08 +0800

    Added navigation to the categories page
    ~ main.js
    ~ main.html
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch skin
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   app/scripts/controllers/main.js
    #	modified:   app/views/main.html
    #

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('halamanHeritageApp')
-  .controller('MainCtrl', function ($scope, categoryList) {
+  .controller('MainCtrl', function ($scope, categoryList, meny) {
     $scope.category = categoryList.query();
     
     $scope.oneAtATime = true;
 
     $scope.linkClass = 'btn btn-large';
+
+    var meny = Meny.create(meny.menyInit());
   });
