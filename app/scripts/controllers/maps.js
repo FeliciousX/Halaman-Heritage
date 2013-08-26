0f213b83163958ff905475cb848ced5d16ae95f3
Infiniafication <isaachawks@gmail.com>
2013-08-26 16:21:39 +0800

    Removed non-working functions

--- app/scripts/controllers/maps.js
+++ app/scripts/controllers/maps.js
@@ -1,45 +1,10 @@
 'use strict';
 
 angular.module('halamanHeritageApp').controller('MapsCtrl', function ($scope) {
-    $scope.myMarkers = [];
     var ll = new google.maps.LatLng(1.5305717, 110.3569404);
     $scope.mapOptions = {
       	center: ll,
       	zoom: 15,
       	mapTypeId: google.maps.MapTypeId.ROADMAP
     }; 
-
-    $scope.addMarker = function($event) {
-    	$scope.myMarkers.push(new google.maps.Marker({
-     		map: $scope.myMap,
-     		position: $event.latLng
-       	}));
-    };
-
-    $scope.setZoomMessage = function(zoom) {
-      	$scope.zoomMessage = 'You just zoomed to '+zoom+'!';
-      	console.log(zoom,'zoomed')
-    };
-
-    $scope.openMarkerInfo = function(marker) {
-      	$scope.currentMarker = marker;
-      	$scope.currentMarkerLat = marker.getPosition().lat();
-      	$scope.currentMarkerLng = marker.getPosition().lng();
-      	$scope.myInfoWindow.open($scope.myMap, marker);
-    };
-
-    $scope.setMarkerPosition = function(marker, lat, lng) {
-      	marker.setPosition(new google.maps.LatLng(lat, lng));
-    };
-
-    //Markers should be added after map is loaded
-    $scope.onMapIdle = function() {
-        if ($scope.myMarkers === undefined){    
-            var marker = new google.maps.Marker({
-                map: $scope.myMap,
-                position: ll
-            });
-            $scope.myMarkers = [marker, ];
-        }
-    };
 });
