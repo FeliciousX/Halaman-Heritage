3758ca4f24122341edfcaff47701a2514dc71291
FeliciousX <feliciousx@gmail.com>
2013-08-29 21:20:38 +0800

    Used angular-google-maps to work with maps instead.
    Still trying to figure out some bugs.
    Still trying to add info window.

--- app/scripts/controllers/categoryDetail.js
+++ app/scripts/controllers/categoryDetail.js
@@ -1,7 +1,7 @@
 'use strict';
 // TODO: beautify page and write tests
 angular.module('halamanHeritageApp')
-	.controller('CategorydetailCtrl', function ($scope, $routeParams, services, meny) {
+	.controller('CategorydetailCtrl', function ($scope, $routeParams, services) {
     $scope.placeId = $routeParams.placeId;
 
     $scope.placeList = services.query({categoryId: $routeParams.categoryId});
