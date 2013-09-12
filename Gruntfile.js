28b9ba620414dbe1b87ad93740685573af91cf78
Pamela Dayne <pam.dayne@gmail.com>
2013-09-13 01:28:51 +0800

    Accordion & Footer overlapping fixed
    Overlapping is fixed (especially when in small screens)

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,8 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        // hostname: 
+        hostname: 0.0.0.0
       },
       livereload: {
         options: {
