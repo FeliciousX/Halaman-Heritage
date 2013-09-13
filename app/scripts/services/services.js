c72607b6c0dd30d13bdb9d0f74cbef3b46fad553
FeliciousX <feliciousx@gmail.com>
2013-09-14 03:08:04 +0800

    Asynchronous now works properly
    ~ Search fixed
    ~ Loading from json files asynchronously and synced!

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,8 +1,71 @@
 'use strict';
 
 angular.module('halamanHeritageApp')
-  .factory('services', function ($resource) {
-    return $resource('places/:categoryId.json', {}, {
-      query: {method:'GET', params:{categoryId:'categories'}, isArray:true}
-    });
+  .factory('services', function ($http, $q) {
+    return {
+			getData: function() {
+								var deffered = $q.defer();
+
+								// TODO: change this to query using POSt on php during production
+								$http.get('places/categories.json').then(function(result) {
+									var parents = result.data;
+									var categories = [];
+
+									angular.forEach(result.data, function(value, key){
+										angular.forEach(value, function(name, tables){
+
+											$http.get('places/' + name + '.json').then(function(result) {
+												var places = result.data;
+												var prettify = name.split('_').join(' ');
+												categories.push(
+													{
+														rawName: name,
+														name: ucwords(prettify),
+														places: places
+													});
+											});
+										});
+									});
+
+									deffered.resolve(categories);
+								});
+
+								return deffered.promise;
+						},
+
+			getDetail: function(category, id) {
+				var deffered = $q.defer();
+
+				$http.get('places/' + category + '.json').then(function(result) {
+					var details;
+					angular.forEach(result.data, function(place, key){
+						if (place.id == id) {
+							details = place;
+							console.log(details);
+						};
+					});
+
+					deffered.resolve(details);
+				});
+
+				return deffered.promise;
+			}
+    }
   });
+
+function ucwords (str) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // +   improved by: Waldo Malqui Silva
+  // +   bugfixed by: Onno Marsman
+  // +   improved by: Robin
+  // +      input by: James (http://www.james-bell.co.uk/)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // *     example 1: ucwords('kevin van  zonneveld');
+  // *     returns 1: 'Kevin Van  Zonneveld'
+  // *     example 2: ucwords('HELLO WORLD');
+  // *     returns 2: 'HELLO WORLD'
+  return (str + '').replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function ($1) {
+    return $1.toUpperCase();
+  });
+}
